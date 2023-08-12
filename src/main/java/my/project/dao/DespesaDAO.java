e7a01891ffbf2e5d386a0324a9cf04c8e22b3015
RaphaelAml <raphaelaml1@outlook.com>
2023-08-12 18:13:07 -0300

    Criado a connection para inserir os dados no mysql, usado o preparedStatement

--- src/main/java/my/project/dao/DespesaDAO.java
+++ src/main/java/my/project/dao/DespesaDAO.java
@@ -1,8 +1,12 @@
 package my.project.dao;
 
+import my.project.infra.ConnectionFactory;
 import my.project.model.Categoria;
 import my.project.model.Despesa;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +16,24 @@ public class DespesaDAO implements IDespesaDAO {
     @Override
     public Despesa save(Despesa despesa) {
 
+        try (Connection connection = ConnectionFactory.getConnection()) {
+
+            String sql = "INSERT INTO Despesas (descricao, valor, data, categoria) VALUES (?,?,?,?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, despesa.getDescricao());
+            preparedStatement.setDouble(2, despesa.getValor());
+            preparedStatement.setDate(3, java.sql.Date.valueOf(despesa.getData()));
+            preparedStatement.setString(4, despesa.getCategoria().toString());
+
+            preparedStatement.executeUpdate();
 
 
-        return null;
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return despesa;
     }
 
     @Override
